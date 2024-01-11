5c353c864942fec2ee6a3ed9cf1381616063ed0d
Gustavo Charamba <gucharamba@gmail.com>
2024-01-11 10:04:55 -0300

    created Position class

---
+++ src/boardgame/Position.java
@@ -0,0 +1,34 @@
+package boardgame;
+
+public class Position {
+
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		
+		this.row = row;
+		this.column = column;
+	}
+	
+	public Integer getRow() {
+		return row;
+	}
+	
+	public Integer getColumn() {
+		return column;
+	}
+	
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
